db36f826048189a2b9199bcee2a5e8445240fb57
MilanP5G <milan_parmar@hotmail.co.uk>
2020-07-17 13:02:30 +0100

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
 /* Your Code Here */
+function createEmployeeRecord(array) {
+  const employee = {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return employee
+}
+
+function createEmployeeRecords(array) {
+  return array.map(createEmployeeRecord)
+}
+
+function createTimeInEvent(dateTime) {
+  let [date, hour] = dateTime.split(' ')
+
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date
+  })
+  return this
+}
+
+function createTimeOutEvent(dateTime) {
+  let [date, hour] = dateTime.split(' ')
+
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date
+  })
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  let timeIn = this.timeInEvents.find(function (e){
+    return e.date === date
+  })
+
+  let timeOut = this.timeOutEvents.find(function (e){
+    return e.date === date
+  })
+
+  return (timeOut["hour"] - timeIn["hour"]) / 100
+}
+
+function wagesEarnedOnDate(date) {
+  return hoursWorkedOnDate.call(this, date) * this.payPerHour
+}
+
+function calculatePayroll(record) {
+  return record.reduce(function(memo, record){
+    return memo + allWagesFor.call(record)
+  }, 0)
+}
+
+function findEmployeeByFirstName(array, firstName){
+  return array.find(function(name){
+    return name.firstName === firstName
+  })
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
