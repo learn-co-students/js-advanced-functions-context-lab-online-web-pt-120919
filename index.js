52e37e33160e5533acdb29c3a6d103cf4c03b0e1
Mely07 <damely.tineo07@gmail.com>
2020-07-18 14:32:46 -0400

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,79 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+function createEmployeeRecord(arr){
+    let obj = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return obj
+}
+
+function createEmployeeRecords(arrays){
+    let newArr = [];
+    // arrays.forEach(function(arr) {
+    //     newArr.push(createEmployeeRecord(arr))
+    // });
+
+    for (let arr of arrays) {
+        newArr.push(createEmployeeRecord(arr))
+    }
+    return newArr;
+}
+
+
+function createTimeInEvent(date){
+    let dateArr = date.split(" ")
+    
+    let timeInEventsObj = {
+        type: "TimeIn",
+        hour: Number(dateArr[1]),
+        date: dateArr[0]
+    }
+
+    this.timeInEvents.push(timeInEventsObj);
+    return this;
+}
+
+function createTimeOutEvent(date){
+    let dateArr = date.split(" ")
+    
+    let timeOutEventsObj = {
+        type: "TimeOut",
+        hour: Number(dateArr[1]),
+        date: dateArr[0]
+    }
+
+    this.timeOutEvents.push(timeOutEventsObj);
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    let timeOut;
+    for (let event of this.timeOutEvents){
+        if (event.date == date)
+            timeOut = event.hour;
+    }  
+   
+    let timeIn;
+    for (let event of this.timeInEvents){
+        if (event.date == date)
+            timeIn = event.hour;
+    } 
+ 
+    return ((timeOut - timeIn) / 100)
+}
+
+function wagesEarnedOnDate(date){
+    let employeeHrs = hoursWorkedOnDate.bind(this);
+    let wage = this.payPerHour
+    return employeeHrs(date) * wage 
+}
+
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
@@ -19,4 +92,24 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let found = srcArray.find(function(record) {
+        if(record.firstName == firstName)
+            return true;
+    });
+
+    return found
+}
+
+function calculatePayroll(employees){
+    // return employees.reduce((accumulator, employee) => {
+    //     return allWagesFor(employee) + accumulator
+    // }, 0)
+
+    return employees.reduce((accumulator, employee) => {
+        let employeeWages = allWagesFor.bind(employee)
+        return employeeWages() + accumulator
+    }, 0)
 }
