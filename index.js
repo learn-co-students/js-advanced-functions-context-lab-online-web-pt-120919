fcd688161484b4218e54132e85051f171a9eea9d
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2020-07-30 11:54:11 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,59 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(employee){
+    return {
+        firstName: employee[0],
+        familyName: employee[1],
+        title: employee[2],
+        payPerHour: employee[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employees){
+    return employees.map(employee => {return createEmployeeRecord(employee)})
+}
+
+function createTimeInEvent(date){
+    let [day, hour] = date.split(" ")
+    let clockIn = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: day
+    }
+
+    this.timeInEvents.push(clockIn)
+    return this
+}
+
+function createTimeOutEvent(date){
+    let [day, hour] = date.split(" ")
+    let clockOut = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: day
+    }
+    this.timeOutEvents.push(clockOut)
+    return this
+}
+
+function hoursWorkedOnDate(day){
+    return ((this.timeOutEvents.find(el => el.date == day ).hour - this.timeInEvents.find(el => el.date == day ).hour)/100)
+}
+
+function wagesEarnedOnDate(day){
+    return hoursWorkedOnDate.call(this, day) * this.payPerHour
+}
+
+function calculatePayroll(employees){
+    return employees.reduce(function(wages, employee){
+        return wages + allWagesFor.call(employee, employee)
+    }, 0)
+}
+
+function findEmployeeByFirstName(employees, firstName){
+    return employees.find(employee => employee.firstName == firstName)
+}
